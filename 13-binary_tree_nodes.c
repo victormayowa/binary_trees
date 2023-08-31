eaaccba5f050494213d77e7381e66da2960d326f
victormayowa <hardmanvicky01@gmail.com>
2023-08-31 05:49:13 -0700

    counts the node with at least 1

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,11 +8,10 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-
-    if (tree->left != NULL || tree->right != NULL)
-        return (1 + binary_tree_nodes(tree->left) + 
+	if (tree == NULL)
+		return (0);
+	if (tree->left != NULL || tree->right != NULL)
+		return (1 + binary_tree_nodes(tree->left) +
 				binary_tree_nodes(tree->right));
 	return (0);
 }
